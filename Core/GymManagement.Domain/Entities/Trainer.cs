4eeb68bef3595c3a0d5ef46760abb3aefb8fcf65
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2022-03-01 09:52:28 +0300

    Add project files.

---
+++ Core/GymManagement.Domain/Entities/Trainer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace GymManagement.Domain.Entities
+{
+    public class Trainer : BaseEntity
+    {
+        public int EmployeeDetailId { get; set; }
+        public EmployeeDetail EmployeeDetail { get; set; }
+
+        public List<Member> Members { get; set; }
+        public int WorkerContractId { get; set; }
+        public WorkerContract WorkerContract { get; set; }
+        public List<Mission> Missions { get; set; }
+    }
+}
+//public class Trainer : Worker
+//{
+//    public List<Member> Members { get; set; }
+
+//}
---
+++ Core/GymManagement.Domain/Entities/Worker.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace GymManagement.Domain.Entities
+{
+    public class Worker : BaseEntity
+    {
+        public int EmployeeDetailId { get; set; }
+        public EmployeeDetail EmployeeDetail { get; set; }
+        public int WorkerContractId { get; set; }
+        public WorkerContract WorkerContract { get; set; }
+        public List<Mission> Missions { get; set; }
+    }
+}
