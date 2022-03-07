f841197c1eaf2644aeca21cd726e76d61f863f08
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2022-03-07 11:18:33 +0300

    TokenGenarator added.

---
+++ Core/GymManagement.Application/Jwt/TokenGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using GymManagement.Domain.Entities;
+using Microsoft.IdentityModel.Tokens;
+
+namespace GymManagement.Application.Jwt
+{
+    public class TokenGenerator
+    {
+
+        public Token CreateToken(Member member, List<string> memberRoles)
+        {
+            var exp = DateTime.Now.AddMinutes(30);
+            var token = new Token {Expiration = exp};
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,member.UserName),
+                new Claim(ClaimTypes.Name,member.Id)
+            };
+
+            foreach (var role in memberRoles)
+            {
+                if (role is not null)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role,Convert.ToString(role)));
+                }
+            }
+
+            //var securityKey = new SymmetricSecurityKey()
+
+
+        }
+    }
+}
--- Core/GymManagement.Application/Services/AuthService.cs
+++ Core/GymManagement.Application/Services/AuthService.cs
@@ -1,21 +1,70 @@
 ﻿using System;
 using System.Threading.Tasks;
+using AutoMapper;
 using GymManagement.Application.Interfaces.ServiceInterfaces;
 using GymManagement.Application.Jwt;
 using GymManagement.Application.ViewModels.MemberViewModel;
+using GymManagement.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace GymManagement.Application.Services
 {
     public class AuthService :IAuthService
     {
-        public Task<bool> Register(MemberRegisterViewModel registerViewModel)
+        private readonly UserManager<Member> _userManager;
+        private readonly SignInManager<Member> _signInManager;
+        private readonly IMapper _mapper;
+
+        public AuthService(IMapper mapper, UserManager<Member> userManager, SignInManager<Member> signInManager)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
-        public Task<Token> Login(MemberLoginViewModel loginViewModel)
+        public async Task<bool> Register(MemberRegisterViewModel registerViewModel)
         {
-            throw new NotImplementedException();
+             var member = _mapper.Map<Member>(registerViewModel);
+
+             var emailCheckMember = await  _userManager.FindByEmailAsync(registerViewModel.Email);
+
+             if (emailCheckMember is not null)
+                 throw new InvalidOperationException("Email Mevcuttur");
+
+             var result = await _userManager.CreateAsync(member, registerViewModel.Password);
+
+
+             //Role Create eklenecek....
+             if (result.Succeeded)
+             {
+                 await _userManager.AddToRoleAsync(member, "Member");
+                 await _signInManager.SignInAsync(member, false);
+                 return true;
+             }
+
+             throw new InvalidOperationException("Kullanıcı kayıt işlemi başarılı değil");
+        }
+
+        public async Task<Token> Login(MemberLoginViewModel loginViewModel)
+        {
+            var memberFind = await _userManager.FindByEmailAsync(loginViewModel.Email);
+
+            if (memberFind is null)
+                throw new InvalidOperationException("Email yanlış");
+
+            var result = await _userManager.CheckPasswordAsync(memberFind, loginViewModel.Password);
+
+            if (!result)
+            {
+                throw new InvalidOperationException("Şifre yanlış");
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(memberFind);
+
+             //var Token = _tokenGenerator.CreteToken(memberFind, userRoles);
+             return null;
+
         }
     }
 }
