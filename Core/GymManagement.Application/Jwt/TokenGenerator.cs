42da901d978eea2df5e6bc21eb2e14861ff39fa1
Güven Barış ÇAKAN <gvnbrs54@gmail.com>
2022-03-07 11:49:37 +0300

    tokengenerator added.

--- Core/GymManagement.Application/Jwt/TokenGenerator.cs
+++ Core/GymManagement.Application/Jwt/TokenGenerator.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 using GymManagement.Domain.Entities;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace GymManagement.Application.Jwt
 {
     public class TokenGenerator
     {
+        private readonly IConfiguration _configuration;
+
+        public TokenGenerator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         public Token CreateToken(Member member, List<string> memberRoles)
         {
@@ -28,9 +37,25 @@ namespace GymManagement.Application.Jwt
                 }
             }
 
-            //var securityKey = new SymmetricSecurityKey()
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
 
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
+            var securityToken = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience:_configuration["Jwt:Audience"],
+                expires:exp,notBefore:DateTime.Now,
+                signingCredentials:credentials,
+                claims:authClaims
+            );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var accessToken = tokenHandler.WriteToken(securityToken);
+
+            token.AccessToken = accessToken;
+            token.RefreshToken = Guid.NewGuid().ToString();
+            return token;
         }
     }
 }
